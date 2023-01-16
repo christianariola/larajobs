a4a42796b122b3e9537db4f0b372674e6cfac7c1
otakuchan <ianbjs@gmail.com>
2023-01-16 02:21:05 -0800

    Add: hero and search form partials

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 
+@include('partials._hero')
+
+@include('partials._search')
+
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 @unless(count($listings) == 0)
@@ -15,9 +19,9 @@
             />
             <div>
                 <h3 class="text-2xl">
-                    <a href="show.html">{{ $listing->title }}</a>
+                    <a href="/listings/{{$listing->id}}">{{ $listing->title }}</a>
                 </h3>
-                <div class="text-xl font-bold mb-4">{{ $listing->company }}/div>
+                <div class="text-xl font-bold mb-4">{{ $listing->company }}
                     <ul class="flex">
                         <li
                             class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
