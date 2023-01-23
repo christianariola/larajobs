25fec9ba7003796a99e754945b6f6f2f17666dac
otakuchan <ianbjs@gmail.com>
2023-01-23 02:15:58 -0800

    Add: create form validation

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -20,5 +21,31 @@ class ListingController extends Controller
         return view('listings.show', [
             'listing' => $listing,
         ]);
+    }
+
+    // Show form to create a listing
+    public function create() {
+        return view('listings.create');
+    }
+
+    // Save a new listing
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email', Rule::unique('listings', 'email')],
+            'description' => 'required',
+            'tags' => 'required',
+        ]);
+
+        // $listing = new Listing();
+        // $listing->title = $request->title;
+        // $listing->description = $request->description;
+        // $listing->tags = $request->tags;
+        // $listing->save();
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,11 @@ use App\Models\Listing;
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
+// Create Listing
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Save Listing
+Route::post('/listings', [ListingController::class, 'store']);
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
-
-
